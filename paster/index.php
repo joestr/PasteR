7e07b78e0b06033ac207ecd109f51f72b3ec9803
Joel Strasser <strasser999@gmail.com>
2018-06-07 16:46:55 +0200

    Updated index.php
    Bootstrap 4 Design

--- paster/index.php
+++ paster/index.php
@@ -1,8 +1,10 @@
 <?php
     
     $paster_name = "PasteR";
-    $paster_webpath = "/paster/";
-    $paster_directory = "/var/www/html/paster/";
+    $paster_url = "https://paster.gstd.eu";
+    $paster_webpath = "/";
+    $paster_directory = "/var/www/paster.gstd.eu/";
+    
     $str_ = "";
     
     // Formularübermittlung
@@ -58,10 +60,10 @@
                                     "\t\t\t<br />\r\n".
                                     "\t\t\t<div class=\"p f\">".$content_streamer."</div>\r\n".
                                     "\t\t\t<br />\r\n".
-                                    "\t\t\t".$paster_name." #<a href=\"http://repo.joestr.xyz".$paster_webpath.$rand."\">".$rand."</a>.\r\n".
+                                    "\t\t\t".$paster_name." #<a href=\"".$paster_url.$paster_webpath.$rand."\">".$rand."</a>.\r\n".
                                     "\t\t\t<br />\r\n".
                                     "\t\t\t<br />\r\n".
-                                    "\t\t\t<a href=\"https://repo.joestr.xyz".$paster_webpath."\">".$paster_name."</a>\r\n".
+                                    "\t\t\t<a href=\"".$paster_url.$paster_webpath."\">".$paster_name."</a>\r\n".
                                     "\t\t\t<br />\r\n".
                                     "\t\t\t<br />\r\n".
                                     "\t\t\tCopyright &copy; <a href=\"https://joestr.xyz\" target=\"_blank\">joestr.xyz</a> - Alle Rechte vorbehalten.\r\n".
@@ -71,7 +73,7 @@
                                     "</html>");
                         fclose($fp);
                         
-                        $str_ = $paster_name." #<a href=\"http://repo.joestr.xyz".$paster_webpath.$rand."\">".$rand."</a> generated.";
+                        $str_ = $paster_name." #<a href=\"".$paster_url.$paster_webpath.$rand."\">".$rand."</a> generated.";
                     }
                 }
             } else {
@@ -94,45 +96,91 @@
 		<meta name="keywords" content="PasteR, PasteR, PasteR, PasteR, PasteR, PasteR, PasteR, PasteR, PasteR, PasteR" />
 		<meta name="description" content="PasteR PasteR PasteR PasteR PasteR PasteR PasteR PasteR PasteR PasteR PasteR PasteR PasteR PasteR PasteR PasteR PasteR PasteR PasteR PasteR" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<title><?php echo $paster_name; ?></title>
-        <style type="text/css">
-            .f{font-size:14px;font-family: "Courier New", monospace;}
-            .m{width:50%;}
-            .p{width:100%;}
-            @media only screen and (max-width:480px){.m{width:90%}}
-            @media only screen and (min-width:480px) and (max-width: 960px){.m{width:90%}}
-            @media only print{.m{width:80%;}}
-        </style>
+        
+		<title><?php echo $paster_name; ?> - Home</title>
+        
+        <link rel="stylesheet" type="text/css"href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" />
+         
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-        <div class="m f" style="text-align:center;margin:auto;">
-            <h1>
-                <?php echo $paster_name; ?>
-            </h1>
-            <!-- Formular -->
-            <form action="index.php" method="post">
-                <textarea type="text" name="title" placeholder="Title" rows="1" class="p f"></textarea>
-                <br />
-                <br />
-                <textarea type="text" name="content" placeholder="Content" rows="25" class="p f"></textarea>
-                <br />
-                <br />
-                <?php echo $num1; ?> + <?php echo $num2; ?> = <input type="hidden" name="result" value="<?php echo sha1(($num1 + $num2)); ?>" />
-                <br />
-                <input type="text"  name="solution" autocomplete="off" class="f" />
-                <br />
-                <br />
-                <input type="submit" name="submit" value="Create" class="f" />
-            </form>
+    
+        <div class="jumbotron text-center" style="margin-bottom: 0;">
+			<h1>PasteR</h1>
+			<p>PHP Paste Tool</p> 
+		</div>
+        
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <a class="navbar-brand" href="#">PasteR</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+			<div class="collapse navbar-collapse" id="collapsibleNavbar">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+                        <a class="nav-link">Home</a>
+                    </li>
+                </ul>
+			</div>
+		</nav>
+        
+        <div class="container-fluid text-center">
             <br />
-            <?php echo "\t\t\t".$str_; ?>
-            <br />
-            <br />
-            <a href="https://repo.joestr.xyz<?php echo $paster_webpath; ?>"><?php echo $paster_name; ?></a>
-            <br />
-            <br />
-            Copyright &copy; <a href="https://joestr.xyz/" target="_blank">joestr.xyz</a> - Alle Rechte vorbehalten.
+            <div class="row content">
+                <div class="col-sm-2"></div>
+                <div class="col-sm-8 text-left">
+                    <?php if(isset($str_) && !empty($str_)): ?>
+                        <p id="topNotification">
+                            <div class="card">
+                                <div class="card-header">Information</div>
+                                <div class="card-body"><span><?php echo $str_; ?></span></div>
+                                <!--<div class="panel-footer"></div>-->
+                            </div>
+                        </p>
+                        <hr />
+                    <?php else: ?>
+                    <?php endif; ?>
+                    <form action="index.php" method="post" class="form-group">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text">
+                                    Title
+                                </div>
+                            </div>
+                            <input type="text" name="title" placeholder="Title" class="form-control" />
+                        </div>
+                        <br />
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text">
+                                    Content
+                                </div>
+                            </div>
+                            <textarea type="text" name="content" placeholder="Content" rows="20" class="form-control" ></textarea>
+                        </div>
+                        <br />
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text">
+                                    <?php echo $num1; ?> + <?php echo $num2; ?> = <input type="hidden" name="result" value="<?php echo sha1(($num1 + $num2)); ?>" />
+                                </div>
+                            </div>
+                            <input type="text"  name="solution" autocomplete="off" class="form-control" />
+                        </div>
+                        <br />
+                        <input type="submit" name="submit" value="Create" class="btn btn-primary" />
+                    </form>
+                </div>
+                <div class="col-sm-2"></div>
+            </div>
             <br />
         </div>
+        
+        <footer class="jumbotron text-center" style="margin-bottom: 0;">
+			Copyright &copy; 2016 - 2018 <a href="https://joestr.xyz/" target="_blank">joestr.xyz</a>
+		</footer>
+        
 	</body>
 </html>
